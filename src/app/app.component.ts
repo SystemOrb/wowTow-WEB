13d91a78df6434eb76bc9b640e1cde1bbb88d597
Carlos Estarita <carlos8estarita@hotmail.com>
2018-09-08 12:41:49 -0400

    debug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,10 @@ import { DOCUMENT } from '@angular/platform-browser';
 import { Location, LocationStrategy, PathLocationStrategy, PopStateEvent } from '@angular/common';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = 0;
+// const didScroll;
+let lastScrollTop = 0;
+const delta = 5;
+const navbarHeight = 0;
 
 @Component({
     selector: 'app-root',
@@ -23,21 +23,23 @@ export class AppComponent implements OnInit {
     url: string;
     @ViewChild(NavbarComponent) navbar: NavbarComponent;
 
-    constructor( private renderer : Renderer, private router: Router, @Inject(DOCUMENT,) private document: any, private element : ElementRef, public location: Location) {}
+    constructor( private renderer: Renderer,
+         private router: Router, @Inject(DOCUMENT) private document: any, private element: ElementRef, public location: Location) {}
 
     @HostListener('window:scroll', ['$event'])
     hasScrolled() {
 
-        var st = window.pageYOffset;
+        const st = window.pageYOffset;
         // Make sure they scroll more than delta
-        if(Math.abs(lastScrollTop - st) <= delta)
+        if (Math.abs(lastScrollTop - st) <= delta) {
             return;
+        }
 
-        var navbar = document.getElementsByTagName('nav')[0];
+        const navbar = document.getElementsByTagName('nav')[0];
 
         // If they scrolled down and are past the navbar, add class .nav-up.
         // This is necessary so you never see what is "behind" the navbar.
-        if (st > lastScrollTop && st > navbarHeight){
+        if (st > lastScrollTop && st > navbarHeight) {
             // Scroll Down
             if (navbar.classList.contains('nav-down')) {
                 navbar.classList.remove('nav-down');
@@ -47,7 +49,7 @@ export class AppComponent implements OnInit {
         } else {
             // Scroll Up
             //  $(window).height()
-            if(st + window.innerHeight < document.body.scrollHeight) {
+            if (st + window.innerHeight < document.body.scrollHeight) {
                 // $('.navbar.nav-up').removeClass('nav-up').addClass('nav-down');
                 if (navbar.classList.contains('nav-up')) {
                     navbar.classList.remove('nav-up');
@@ -59,22 +61,23 @@ export class AppComponent implements OnInit {
         lastScrollTop = st;
     };
     ngOnInit() {
-        var navbar : HTMLElement = this.element.nativeElement.children[0].children[0];
+        const navbar: HTMLElement = this.element.nativeElement.children[0].children[0];
         if (this.location.path() !== '/sections') {
-            this.location.subscribe((ev:PopStateEvent) => {
+            this.location.subscribe((ev: PopStateEvent) => {
                 this.lastPoppedUrl = ev.url;
             });
-             this.router.events.subscribe((event:any) => {
+             this.router.events.subscribe((event: any) => {
                 if (event instanceof NavigationStart) {
-                   if (event.url != this.lastPoppedUrl)
+                   if (event.url !== this.lastPoppedUrl) {
                        this.yScrollStack.push(window.scrollY);
+                   }
                } else if (event instanceof NavigationEnd) {
-                   if (event.url == this.lastPoppedUrl) {
+                   if (event.url === this.lastPoppedUrl) {
                        this.lastPoppedUrl = undefined;
                        window.scrollTo(0, this.yScrollStack.pop());
-                   }
-                   else
+                   } else {
                        window.scrollTo(0, 0);
+                   }
                }
             });
         }
@@ -82,16 +85,20 @@ export class AppComponent implements OnInit {
         this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
             this.navbar.sidebarClose();
 
-            this.renderer.listenGlobal('window', 'scroll', (event) => {
+            this.renderer.listenGlobal('window', 'scroll', (): void => {
                 const number = window.scrollY;
-                var _location = this.location.path();
+                let _location = this.location.path();
                 _location = _location.split('/')[2];
                 if (this.location.path() !== '/sections') {
 
                     if (number > 150 || window.pageYOffset > 150) {
                         // add logic
                         navbar.classList.remove('navbar-transparent');
-                    } else if (_location !== 'addproduct' && _location !== 'blogposts' && _location !== 'discover' && _location !== 'contactus' && _location !== 'login' && _location !== 'register' && _location !== 'search' && this.location.path() !== '/nucleoicons') {
+                    } else if (_location !== 'addproduct' && _location !== 'blogposts'
+                    &&  _location !== 'discover' && _location !== 'contactus'
+                    && _location !== 'login' && _location !== 'register'
+                    && _location !== 'search'
+                    && this.location.path() !== '/nucleoicons') {
                         // remove logic
                         navbar.classList.add('navbar-transparent');
                     }
@@ -99,27 +106,26 @@ export class AppComponent implements OnInit {
             });
         });
 
-        var ua = window.navigator.userAgent;
-        var trident = ua.indexOf('Trident/');
+        const ua = window.navigator.userAgent;
+        const trident = ua.indexOf('Trident/');
         if (trident > 0) {
             // IE 11 => return version number
-            var rv = ua.indexOf('rv:');
+            const rv = ua.indexOf('rv:');
             var version = parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
         }
         if (version) {
-            var body = document.getElementsByTagName('body')[0];
+            const body = document.getElementsByTagName('body')[0];
             body.classList.add('ie-background');
 
         }
         this.hasScrolled();
     }
     removeFooter() {
-        var titlee = this.location.prepareExternalUrl(this.location.path());
+        let titlee = this.location.prepareExternalUrl(this.location.path());
         titlee = titlee.slice( 1 );
-        if(titlee === 'signup' || titlee === 'nucleoicons'){
+        if (titlee === 'signup' || titlee === 'nucleoicons'){
             return false;
-        }
-        else {
+        } else {
             return true;
         }
     }
